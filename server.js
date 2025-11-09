a2e3dd50f0b779fa952355a02a9c64f1f046d76c
sujalbirari <sujalbirari@gmail.com>
2025-11-09 20:09:22 +0530

    feat(prod): allow cross origin cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 require('dotenv').config()
 const morgan = require('morgan')
+const cors = require('cors');
 
 const dbConnect = require('./config/db');
 const authRoutes = require('./routes/auth');
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan('dev'));
+app.use(cors());
 app.use('/api/auth', authRoutes);
 app.use('/api/tales', taleRoutes);
 
