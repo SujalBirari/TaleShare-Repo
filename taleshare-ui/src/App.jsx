cb90e5caa53d9518dd35ba6d4df7928e6ed80dcf
sujalbirari <sujalbirari@gmail.com>
2025-11-22 15:30:14 +0530

    core(ui): setting tailwind css in project

--- taleshare-ui/src/App.jsx
+++ taleshare-ui/src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 
 function App() {
-
+    return (
+        <>
+            <h1 className='text-7xl text-center text-blue-400'>Hello World</h1>
+        </>
+    )
 }
 
-export default App
+export default App;
